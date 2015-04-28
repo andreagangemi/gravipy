e1d862fdac965bd8670bfbed8875a877d4c1ea0a
Andrea Gangemi <andrea.gangemi@gmail.com>
2015-04-28 23:54:28 +0200

    first release

---
+++ gravipy.py
@@ -0,0 +1,17 @@
+__author__ = 'Andrea Gangemi'
+__Version__ = '0.0.1'
+
+
+# http://www.braeunig.us/space/orbmech.htm
+
+class Orbit():
+    def __init__(self):
+        pass
+
+
+class Satellite(Orbit):
+    def __init__(self):
+        pass
+
+
+
---
+++ gravipy_constants.py
@@ -0,0 +1,4 @@
+__author__ = 'Andrea Gangemi'
+__Version__ = '0.0.1'
+
+g_const = 9.80665
