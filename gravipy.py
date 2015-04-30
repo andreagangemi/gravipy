e04be12ac59bc52145831c0cb3c357c987bda1a1
Andrea Gangemi <andrea.gangemi@gmail.com>
2015-05-01 01:29:45 +0200

    added orbital parameter dictionary

--- gravipy.py
+++ gravipy.py
@@ -6,8 +6,9 @@ __Version__ = '0.0.1'
 
 class Orbit():
     def __init__(self):
+        self.orbit_parameters = {'major_semiaxis': 0, 'eccentricity': 0, 'inclination': 0, 'periapsis_arg': 0,
+                                 'periapsis_passage': 0, 'ascending_node_longitude': 0}
         pass
-
 
 class Satellite(Orbit):
     def __init__(self):
